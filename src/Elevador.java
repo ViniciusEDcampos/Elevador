c6a38a3132763e4afa4c0cb5115d0102279e3371
ViniciusEDcampos <viniciuseduardo1403@gmail.com>
2025-05-19 13:21:33 -0300

    Melhoria da Formatação do programa, e testes de erros.

--- src/Elevador.java
+++ src/Elevador.java
@@ -29,6 +29,7 @@ public class Elevador {
     }
 
     public int getAndar_atual() {
+
         return andar_atual;
     }
 
--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,50 @@
 public class Main {
     public static void main(String[] args) {
 
-    Elevador elevadorBanco = new Elevador(0,10,0,6);
+    Elevador elevadorBanco = new Elevador(0,10,0,4);
 
+        //Entrada de 6 pessoas no elevador
+        elevadorBanco.entrar_elevador(10);
 
-        elevadorBanco.entrar_elevador(3);
-        elevadorBanco.entrar_elevador(3);
-        System.out.println("Andar atual" + elevadorBanco.getAndar_atual());
+        //Entrada de 2 pessoas(tratamento de erro para testar de a capacidade trava na 10)
+        elevadorBanco.entrar_elevador(2);
+
+        //identificação de qual andar está o elevador
+        System.out.println("Andar atual: " + (elevadorBanco.getAndar_atual() == 0 ? "Térreo" : elevadorBanco.getAndar_atual()));
+
+        //identificação de quantas pessoas estão no elevador
         System.out.println("Quantidade de pessoas no elevador " + elevadorBanco.getPessoas_presentes());
+
+        //metodo para descer um andar(tentativa de erro, para descer abaixo do terreo)
         elevadorBanco.descer_andar_elevador();
-        System.out.println("Andar atual " + elevadorBanco.getAndar_atual());
+
+        //metodo para subir um andar
         elevadorBanco.subir_andar_elevador();
-        System.out.println("Andar atual " + elevadorBanco.getAndar_atual());
+
+        //impressão de qual andar o elevador esta
+        System.out.println("Andar atual: " + elevadorBanco.getAndar_atual());
+
+        //subindo mais um andar com o metodo
         elevadorBanco.subir_andar_elevador();
-        System.out.println("Andar atual " + elevadorBanco.getAndar_atual());
+
+        //impressão de qual andar o elevador esta
+        System.out.println("Andar atual: " + elevadorBanco.getAndar_atual());
+
+        //subindo mais um andar com o metodo
         elevadorBanco.subir_andar_elevador();
-        System.out.println("Andar atual " + elevadorBanco.getAndar_atual());
-        System.out.println("Quantidade de pessoas no elevador " + elevadorBanco.getPessoas_presentes());
+
+        //impressão de qual andar o elevador esta
+        System.out.println("Andar atual: " + elevadorBanco.getAndar_atual());
+
+        //saida de 2 pessoas do elevador
         elevadorBanco.sair_elevador(2);
+
+        // pegando o valor de quantas pessoas estão ocupando o elevador
+        System.out.println("Quantidade de pessoas no elevador: " + elevadorBanco.getPessoas_presentes());
+
+
+        //tentativa de subir mais andares do que o possivel(tentativa de erro)
+        elevadorBanco.subir_andar_elevador();
 
     }
 }
